e5743bc258536a56e83dff35b7f22e2647cf1236
Luan Argolo <ipsluan@icloud.com>
2023-04-06 13:45:23 -0300

    fix(eslint): add config for endOfLine

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,5 +12,11 @@ module.exports = {
   rules: {
     semi: [2, 'never'],
     'no-extra-boolean-cast': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
   },
 }
